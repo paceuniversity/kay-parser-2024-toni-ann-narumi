766ef30af2fda03a1a93e24de73d452369ab5ffa
mcdanieltoniann <mcdanieltoniann@gmail.com>
2024-11-25 14:45:40 -0500

    test

--- src/main/java/com/scanner/project/ConcreteSyntax.java
+++ src/main/java/com/scanner/project/ConcreteSyntax.java
@@ -135,10 +135,7 @@ public class ConcreteSyntax {
 		// Block --> '{' Statements '}'
 		Block b = new Block();
 		while (!token.getValue().equals("}")) {
-			Statement s = statement();
-			if (!(s instanceof Skip)) {
-				b.blockmembers.addElement(s);
-			}
+			b.blockmembers.addElement(statement());
 		}
 		return b;
 	}
